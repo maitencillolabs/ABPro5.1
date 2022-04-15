0ba762e54854ba8f7d8681338b72c66e7d5fa178
Ariel Abarca P <85913034+Ariel-Silver@users.noreply.github.com>
2022-04-15 19:01:22 -0400

    Add files via upload
    Favoritos añadidos

---
+++ Abpro 5 Viernes modulo 5/abpro5.1/script.js
@@ -0,0 +1,134 @@
+function traer() {
+    fetch("tabla.json")
+        .then(resultadoTabla => resultadoTabla.json()) // FORMATO QUE VAMOS A RECIBIR NUESTRA INFORMACIÓN
+        .then(datos => {
+            tabla(datos)
+            console.log (datos)
+
+        })
+    }
+let verPost=document.getElementById('postBlog');
+function tabla(datos){
+    verPost.innerHTML = ''
+    datos.map (e=>{
+    verPost.innerHTML+= `<div class="card">
+    <div class="card-body">
+      <h4 class="card-title">${e.titulo}</h4>
+      <h6 class="card-subtitle mb-2 text-muted">${e.subtitulo}</h6>
+      <p class="card-text">${e.text}.</p>
+      <a href="${e.link}" class="card-link">Conoce Más</a>
+
+      <div class="container">
+        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#Modal">Modal</button>
+        <!-- Modal -->
+        <div class="modal fade" id="Modal" role="dialog">
+            <div class="modal-dialog">
+                <!-- Contenido del modal-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="Mas Informacion">&times;</button>
+                        <br>
+                        <h4 class="modal-title"></h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="card-body">
+                        <h4 class="card-title">${e.subtitulo}</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">${e.titulo}</h6>
+                        <p class="card-text">${e.text}.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button onclick="toggle(this)" type="button" class="btn btn-success">Agregar a Favoritos</button>
+    </div>
+    </div>`})
+}
+
+//Funcion toggle favoritos
+
+function toggle(e) {
+  let txt = e.innerText;
+  e.innerText = txt == 'Remover de Favoritos' ? 'Agregar a Favoritos' : 'Remover de Favoritos';
+}
+
+function buscar() {
+    fetch("tabla.json")
+        .then(resultadoTabla => resultadoTabla.json()) // FORMATO QUE VAMOS A RECIBIR NUESTRA INFORMACIÓN
+        .then(e => {
+           buscarPost(e)
+            console.log (e)
+
+        })
+    }
+
+ function buscarPost(e) {
+    verPost.innerHTML=""
+    const buscarPost = document.getElementById("buscarF").value
+    if(buscarPost == "")
+    {
+      alert("Su Busqueda está vacia")
+    }
+else if (!isNaN(buscarPost)) 
+  {
+    alert("no puede ingresar numeros");
+ }
+
+
+ else{const nombrePost = buscarPost.toLowerCase()
+  const filtrarPost = e.filter(e => {
+    const nuevoNombre = e.titulo
+    const transformarNombre = nuevoNombre.toLowerCase()
+    return transformarNombre == "" ?  traer() : transformarNombre.includes(nombrePost)
+  })
+  if(filtrarPost==""){
+    alert("No se encuentra ningun Post")}
+ 
+    filtrarPost.map((e) => {
+      verPost.innerHTML += `<div class="card">
+    <div class="card-body">
+      <h4 class="card-title">${e.titulo}</h4>
+      <h6 class="card-subtitle mb-2 text-muted">${e.subtitulo}</h6>
+      <p class="card-text">${e.text}.</p>
+      <a href="${e.link}" class="card-link">Conoce Más</a>
+      <button id="agregar" onclick="agregarFav()" type="button" class="btn btn-success">Agregar a Favoritos</button>
+    </div>
+    </div>`
+    })}
+  }
+//agregar a favoritos
+
+const addToFavoritosButtons = document.querySelectorAll('.btn-success');
+addToFavoritosButtons.forEach((addToFavoritosButton) => {
+    addToFavoritosButton.addEventListener('click', addToCartClicked);
+});
+
+const comprarButton = document.querySelector('.comprarButton');
+comprarButton.addEventListener('click', comprarButtonClicked);
+const verFav = document.getElementById("favoritos");
+
+
+function addToFavClicked(event) {
+    const button = event.target;
+    const item = button.closest('.item');
+  
+    const itemTitle = item.querySelector('.item-title').textContent;
+    const itemPrice = item.querySelector('.item-price').textContent;
+    const itemImage = item.querySelector('.item-image').src;
+  
+    addItemToShoppingCart(itemTitle, itemPrice, itemImage);
+  }
+const postTitle=
+  function agregarFav(){
+      verFav.innerHTML += `<div class="card text-white bg-success mb-3" style="max-width: 20rem;">
+      <div class="card-header">${e.titulo}</div>
+      <div class="card-body">
+        <h4 class="card-title">${e.subtitulo}</h4>
+        <p class="card-text">${e.text}</p>
+      </div>
+    </div>`
+
+  }
